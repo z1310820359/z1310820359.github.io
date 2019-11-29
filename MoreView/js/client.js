0286b256f85c1acda990ca1acfc89f0a230b6054
z1310820359 <1310820359@qq.com>
2019-11-29 17:31:39 +0800

    Initial commit

--- MoreView/js/client.js
+++ MoreView/js/client.js
@@ -1,4 +1,8 @@
 'use strict'
+var audioSource = document.querySelector("select#audioSource");
+var audioOutput = document.querySelector("select#audioOutput");
+var videoSource = document.querySelector("select#videoSource");
+
 if(!navigator.mediaDevices||
  !navigator.mediaDevices.enumerateDevices){
 	console.log('enumerateDevices is not supported');
@@ -9,6 +13,16 @@ function gotDevices(deviceInfos){
 	deviceInfos.forEach( function(deviceInfo){
 		console.log(deviceInfo.kind + ':' + deviceInfo.label +"; id = " +
 		+deviceInfo	.deviceId + ": groupId =" + deviceInfo.groupId);
+		var option = createElements('option');
+		option.text = deviceInfo.label;
+		option.value = deviceInfo.deviceId;
+		if(deviceInfo.kind === 'audioSource'){
+			audioSource.appendChild(option);
+		}else if(deviceInfo.kind === 'audioOutput'){
+			audioOutput.appendChild(option);
+		}else if(deviceInfo.kind === 'videoSource'){
+			videoSource.appendChild(option);
+		}
 	})
 }
 function handleError(err){
