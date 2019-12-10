a19329b21fdf5e72869ef0e685c7e321f73dcdd9
z1310820359 <1310820359@qq.com>
2019-12-10 11:50:56 +0800

    Initial commit

--- webrtc.js
+++ webrtc.js
@@ -1,121 +1,199 @@
-if (!location.hash) {
-    location.hash = Math.floor(Math.random() * 0xFFFFFF).toString(16);
+
+var connection = new WebSocket('ws://localhost:3002'),
+name = "";
+var loginPage = document.querySelector('#login-page'),
+usernameInput = document.querySelector('#username'),
+loginButton = document.querySelector('#login'),
+callPage = document.querySelector('#call-page'),
+theirUsernameInput = document.querySelector('#their-username'),
+callButton = document.querySelector('#call'),
+hangUpButton = document.querySelector('#hang-up');
+callPage.style.display = "none";
+// Login when the user clicks the button
+loginButton.addEventListener("click",
+function(event) {
+    name = usernameInput.value;
+    if (name.length > 0) {
+        send({
+            type: "login",
+            name: name
+        });
+    }
+});
+connection.onopen = function() {
+console.log("Connected");
+};
+// Handle all messages through this callback
+connection.onmessage = function(message) {
+//alert(JSON.stringify(message.data));
+console.log("Got message", message.data);
+var data = JSON.parse(message.data);
+switch (data.type) {
+    case "login":
+        onLogin(data.success);
+        break;
+    case "offer":
+        onOffer(data.offer, data.name);
+        break;
+    case "answer":
+        onAnswer(data.answer);
+        break;
+    case "candidate":
+        onCandidate(data.candidate);
+        break;
+    case "leave":
+        onLeave();
+        break;
+    default:
+        break;
 }
-// 获取房间号
-var roomHash = location.hash.substring(1);
-// 你也可以自己创建
-var drone = new ScaleDrone('UfOlfBFyff9S62Cm');
-// 房间名必须以 'observable-'开头
-var roomName = 'observable-' + roomHash;
+};
+connection.onerror = function(err) {
+console.log("Got error", err);
+};
+// Alias for sending messages in JSON format
+function send(message) {
+if (connectedUser) {
+    message.name = connectedUser;
+}
+connection.send(JSON.stringify(message));
+};
+function onLogin(success) {
+if (success === false) {
+    alert("Login unsuccessful, please try a different name.");
+} else {
+    loginPage.style.display = "none";
+    callPage.style.display = "block";
+    // Get the plumbing ready for a call
+    startConnection();
+}
+};
+callButton.addEventListener("click",
+function() {
+    var theirUsername = theirUsernameInput.value;
+    if (theirUsername.length > 0) {
+        startPeerConnection(theirUsername);
+    }
+});
+hangUpButton.addEventListener("click",
+function() {
+    send({
+        type: "leave"
+    });
+    onLeave();
+});
+function onOffer(offer, name) {
+connectedUser = name;
+yourConnection.setRemoteDescription(new RTCSessionDescription(offer));
+yourConnection.createAnswer(function(answer) {
+        yourConnection.setLocalDescription(answer);
+        send({
+            type: "answer",
+            answer: answer
+        });
+    },
+    function(error) {
+        alert("An error has occurred");
+    });
+}
+function onAnswer(answer) {
+yourConnection.setRemoteDescription(new RTCSessionDescription(answer));
+}
+function onCandidate(candidate) {
+yourConnection.addIceCandidate(new RTCIceCandidate(candidate));
+}
+function onLeave() {
+connectedUser = null;
+theirVideo.src = null;
+yourConnection.close();
+yourConnection.onicecandidate = null;
+yourConnection.onaddstream = null;
+setupPeerConnection(stream);
+}
+
+function canGetUserMediaUse() {
+return !!(navigator.mediaDevices.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+}
+
+function getUserMedia(constrains, success, error){
+if(navigator.mediaDevices.getUserMedia){
+    navigator.mediaDevices.getUserMedia(constrains).then(success).catch(error);
+}else if(navigator.webkitGetUserMedia){
+    navigator.webkitGetUserMedia(constrains).then(success).catch(error);
+}else if (navigator.mozGetUserMedia){
+    navigator.mozGetUserMedia(constrains).then(success).catch(error);
+} else if (navigator.msGetUserMedia){
+    navigator.msGetUserMedia(constrains).then(success).catch(error);
+}
+}
+
+function hasRTCPeerConnection() {
+window.RTCPeerConnection = window.RTCPeerConnection || window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
+window.RTCSessionDescription = window.RTCSessionDescription || window.webkitRTCSessionDescription || window.mozRTCSessionDescription;
+window.RTCIceCandidate = window.RTCIceCandidate || window.webkitRTCIceCandidate || window.mozRTCIceCandidate;
+return !! window.RTCPeerConnection;
+}
+var yourVideo = document.querySelector('#yours'),
+theirVideo = document.querySelector('#theirs'),
+yourConnection,
+connectedUser,
+stream;
+function startConnection() {
+if (canGetUserMediaUse()) {
+    getUserMedia({
+            video: true,
+            audio: true
+        },
+        function(myStream) {
+            stream = myStream;
+            yourVideo.srcObject = stream;
+            if (hasRTCPeerConnection()) {
+                setupPeerConnection(stream);
+            } else {
+                alert("Sorry, your browser does not support WebRTC.");
+            }
+        },
+        function(error) {
+            console.log(error);
+        });
+} else {
+    alert("Sorry, your browser does not support WebRTC.");
+}
+}
+function setupPeerConnection(stream) {
 
 var configuration = {
-    iceServers: [{
-        urls: 'stun:stun.l.google.com:19302' // 使用谷歌的stun服务
+    "iceServers": [{
+        "url": "stun:stun.1.google.com:19302"
     }]
 };
-function onError(error) {
-    console.error(error);
+yourConnection = new RTCPeerConnection(configuration);
+// Setup stream listening
+yourConnection.addStream(stream);
+yourConnection.onaddstream = function(e) {
+    theirVideo.srcObject = e.stream;
 };
-function onSuccess(error) {
+// Setup ice handling
+yourConnection.onicecandidate = function(event) {
+    if (event.candidate) {
+        send({
+            type: "candidate",
+            candidate: event.candidate
+        });
+    }
+}
+}
+function startPeerConnection(user) {
+connectedUser = user;
+// Begin the offer
+yourConnection.createOffer(function(offer) {
+        send({
+            type: "offer",
+            offer: offer
+        });
+        yourConnection.setLocalDescription(offer);
+    },
+    function(error) {
+        alert("An error has occurred.");
+    });
 };
-// 通过Scaledrone发送信令消息
-function sendMessage(message) {
-    drone.publish({
-        room: roomName,
-        message
-    });
-}
-
-drone.on('open', function(error){
-    if (error) { return console.error(error);}
-
-    room = drone.subscribe(roomName);
-    room.on('open', function(error){
-        if (error) {onError(error);}
-    });
-
-    // 已经链接到房间后，就会收到一个 members 数组，代表房间里的成员
-    // 这时候信令服务已经就绪
-    room.on('members', function(members){
-        console.log('MEMBERS', members);
-
-        // 如果你是第二个链接到房间的人，就会创建offer
-        var isOfferer = members.length === 2;
-        startWebRTC(isOfferer);
-    });
-});
-
-function startWebRTC(isOfferer) {
-    
-    pc = new RTCPeerConnection(configuration);
-    // 当本地ICE Agent需要通过信号服务器发送信息到其他端时
-    // 会触发icecandidate事件回调
-    pc.onicecandidate = function(event){
-        if (event.candidate) {
-            sendMessage({ 'candidate': event.candidate });
-        }
-    };
-// 如果用户是第二个进入的人，就在negotiationneeded 事件后创建sdp
-if (isOfferer) {
-    // onnegotiationneeded 在要求sesssion协商时发生
-    pc.onnegotiationneeded = function() {
-        // 创建本地sdp描述 SDP (Session Description Protocol) session描述协议
-        pc.createOffer().then(localDescCreated).catch(onError);
-    };
-}
-
-
-pc.onaddstream = function(event){
-    remoteVideo.srcObject = event.stream;
-};
-
-// 获取本地媒体流
-
-navigator.mediaDevices.getUserMedia({
-    audio: true,
-    video: true,
-}).then( function(stream) {
-    // 将本地捕获的视频流装载到本地video中
-    localVideo.srcObject = stream;
-
-    // 将本地流加入RTCPeerConnection 实例中 发送到其他端
-    pc.addStream(stream);
-}, onError);
-
-var recorder = new MediaRecorder(stream2);
-
-function start(){
-    recorder.start();
-}
-
-function stop(){
-    recorder.stop();
-}
-
-room.on('data', function(message, client){
-    // 消息是我自己发送的，则不处理
-    if (client.id === drone.clientId) {
-        return;
-    }
-    if (message.sdp) {
-        // 设置远程sdp, 在offer 或者 answer后
-        pc.setRemoteDescription(new RTCSessionDescription(message.sdp), function(){
-            // 当收到offer 后就接听
-            if (pc.remoteDescription.type === 'offer') {
-                pc.createAnswer().then(localDescCreated).catch(onError);
-            }
-        }, onError);
-    }
-    else if (message.candidate) {
-        // 增加新的 ICE canidatet 到本地的链接中
-        pc.addIceCandidate(
-            new RTCIceCandidate(message.candidate), onSuccess, onError
-        );
-    }
-});
-}
-function localDescCreated(desc) {
-    pc.setLocalDescription(desc, function(){
-        sendMessage({ 'sdp': pc.localDescription });
-    },onError);
-}
